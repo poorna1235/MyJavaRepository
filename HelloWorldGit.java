61d5e1a9dd7d1d811e06e0a56715dae4caecde91
poorna_fondekar <poorna_fondekar@persistent.co.in>
2018-07-26 14:54:51 +0530

    display date method improved

--- HelloWorldGit.java
+++ HelloWorldGit.java
@@ -1,10 +1,11 @@
 class HelloWorld{
 
-void displayDate(){
-sysout("display todays date:-");
-}
- public static void main(String args)
-{
+       void displayDate(){
+
+               system.out.println("display todays date:-");
+         }
+ public static void main(String args){ 
+
        system.out.println("hello world...welcome to git");
-}
+  }
  }
